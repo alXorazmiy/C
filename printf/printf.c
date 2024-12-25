06f697c2dca3999552087bdb9698a714243e907a
Nila Yoshie <nilayosh@kh-a7.tash.21-school.uz>
2024-12-25 20:10:02 +0500

    commit

---
+++ printf/printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>  //C tilidagi standart kutubxona
+
+
+int main () {
+    printf("Hello Wolrd"); //consolega malumot chiqazuvchi funksiya
+    return 0; // dasturni tugatishni bildirish
+}
---
+++ variables/int.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+    int variable_1 = 3; // int toifasi xotiradan 2 yoki 4 bayt joy oladi va butun sonlarni saqlaydi
+    printf("%d\n", variable_1); // buyerda console ga 3 qiymati chiqadi
+    variable_1 = 4; // variable_1 qiymatini endi 4 ga o'zgartirdik
+    printf("%d\n", variable_1); // consolega 4 qiymati chiqadi
+
+    return 0;
+
+}
