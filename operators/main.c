270b11166e8509bde352479347d10efcbbcf26e0
Nila Yoshie <nilayosh@kh-a7.tash.21-school.uz>
2024-12-26 16:00:26 +0500

    operatorlar

---
+++ operators/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () {
+    printf("2 + 2 = %d\n", 2 + 2);
+    printf("2 - 2 = %d\n", 2 - 2);
+    printf("2 * 2 = %d\n", 2 * 2);
+    printf("2 / 2 = %d\n", 2 / 2);
+    printf("2 %% 2 = %d\n", 2 % 2);
+    return 0;
+}
