030e3c135c2cd42e4eea337a409bd59253ce38da
Nila Yoshie <nilayosh@kh-a7.tash.21-school.uz>
2024-12-27 18:38:48 +0500

    if else

---
+++ if else/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main () {
+    int a = 4, b = 3;
+    if (a > b) {
+        printf("a son b sondan kotta\n");
+    } else {
+        printf("b son a sondan kotta\n");
+    }
+    return 0;
+}
