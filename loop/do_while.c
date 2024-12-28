fffb93239c9f5b69012a39b78bf092b33b423843
Nila Yoshie <nilayosh@kh-a7.tash.21-school.uz>
2024-12-28 09:57:47 +0500

    do while sikl operatori

---
+++ loop/do_while.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+    do {
+        printf("i = %d\n", i);
+        i--;
+    } while (i > 0);
+    return 0;
+}
+
+// do while operatori 1 marta bo'sayam shart bajarilishini taminlaydi.
