08794d9c8dd7ff5e992c5768359731ad13e2a76f
alXorazmiy <alxorazmiy1997@gmail.com>
2025-01-09 16:42:40 +0500

    linked listga value qo'shish

---
+++ linked list/linked_list_add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int x;
+    struct Node *next;
+} Node;
+
+
+void output(Node *root) {
+    while (root != NULL){
+        printf("%d\n", root->x);
+        root = root->next;
+    }
+}
+
+
+void insert_end(Node *root, int value) {
+    while (root->next != NULL){
+        root = root->next;
+    }
+    root->next = malloc(sizeof(Node));
+    root->next->x = value;
+    root->next->next = NULL;
+}
+
+int main() {
+    Node root;
+    root.x = 15;
+    root.next = NULL;
+    insert_end(&root, 5);
+    output(&root);
+    
+    return 0;
+}
