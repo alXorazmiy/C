fc96b32c5afdd65cc611b6555dc92b9c74c86dca
alXorazmiy <alxorazmiy1997@gmail.com>
2025-01-13 14:57:55 +0500

    linked listning boshiga qiymat qo'shish

---
+++ linked list/linked_list_addbegin.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int x;
+    struct Node *next;
+} Node;
+
+
+void output(Node **node) {
+    Node *nodes = *node;
+    while (nodes != NULL){
+        printf("%d\n", nodes->x);
+        nodes = nodes->next;
+    }
+}
+
+
+void insert_end(Node **node, int value) {
+    Node *new_node = malloc(sizeof(Node));
+    if (new_node == NULL) {
+        exit(1);
+    }
+
+    new_node->next = NULL;
+    new_node->x = value;
+
+    if (*node == NULL) {
+        *node = new_node;
+        return;
+    }
+
+    Node *current_node = *node;
+    while (current_node->next != NULL){
+        current_node = current_node->next;
+    }
+    current_node->next = new_node;
+}
+
+
+void insert_begin(Node **node, int value) {
+    Node *new_node = malloc(sizeof(Node));
+    if (new_node == NULL) {
+        exit(1);
+    }
+
+    new_node->x = value;
+    new_node->next = *node;
+    *node = new_node;
+}
+
+void deollacate(Node **node) {
+    Node *current_node = *node;
+    while (current_node != NULL){
+        Node *item = current_node;
+        current_node = current_node->next;
+        free(item);
+    }
+    *node = NULL;
+    
+}
+
+int main() {
+    Node *node = NULL;
+    insert_end(&node, 5);
+    insert_end(&node, -1);
+    insert_end(&node, 3);
+    insert_begin(&node ,1);
+    output(&node);
+    deollacate(&node);
+    
+    return 0;
+}
