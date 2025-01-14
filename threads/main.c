0385a46c15ea547cb2ef219883a2735c2c87beea
alXorazmiy <alxorazmiy1997@gmail.com>
2025-01-14 11:26:16 +0500

    assinxron dasturlash

---
+++ threads/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void *routine() {
+    printf("Test form threads\n");
+    sleep(3);
+    printf("Ending thread\n");
+}
+
+int main() {
+    pthread_t t1, t2;
+    pthread_create(&t1, NULL, &routine, NULL);
+    pthread_create(&t2, NULL, &routine, NULL);
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    return 0;
+}
